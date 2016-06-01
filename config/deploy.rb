5a113c214f8cd080555423f1075ec285625e1a08
Donavon McDonald <donavon_mcdonald@hotmail.com>
2016-06-01 10:20:19 -0400

    add script to run schema:load in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,26 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'   ## TODO: set your version of cap here
 
+namespace :deploy do
+  namespace :db do
+    desc "Load the database schema if needed"
+    task load: [:set_rails_env] do
+      on primary :db do
+        if not test(%Q[[ -e "#{shared_path.join(".schema_loaded")}" ]])
+          within release_path do
+            with rails_env: fetch(:rails_env) do
+              execute :rake, "db:schema:load"
+              execute :touch, shared_path.join(".schema_loaded")
+            end
+          end
+        end
+      end
+    end
+  end
+
+  before "deploy:migrate", "deploy:db:load"
+end
+
 set :application, 'rentuspace'
 set :repo_url, 'git@github.com:aakkmodede/sharetribe.git'
 
